2b7fd987982eca28aae69b009abf1f6818da7e2a
Lilloaf <adenaikeyomi@gmail.com>
2018-11-18 21:05:07 +0000

    Syntax highlighting working!

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,6 @@
 //Imports
 const mainMenu = require("./components/menu");
 var ipcRenderer = require('electron').ipcRenderer;
-const CodeMirror = require("codemirror");
 
 //Main UI
 exports.codeWindow = document.getElementById("codeWindow");
@@ -15,41 +14,26 @@ var languageModeDialog = document.getElementById("languageModeDialog");
 var languageListElements = document.querySelectorAll("li");
 
 //HTML Stylesheet
-currentStyleSheet = document.getElementById("codeMirrorThemeCss");
-
-//Init Terminal
-setTerminal = function (){
-    var term = new Terminal();
-    term.open(document.getElementById('terminal'));
-    term.on("data",function(data){
-        term.write(data);
-    });
-}
-
+var currentStyleSheet = document.getElementById("codeMirrorThemeCss");
 //Init Editor 
 function setEditor(){
-    var config = {
+    exports.editableCodeMirror = CodeMirror.fromTextArea(codeWindow, {
         lineNumbers: true,
-       
-        
-    };
-    exports.editableCodeMirror = CodeMirror.fromTextArea(codeWindow, config);
-    
+        matchBrackets:true,
+    });
     //Setting preferences
     ipcRenderer.on("selected-theme",function(event,payload){
         setStylesheet(payload);
         exports.editableCodeMirror.setOption("theme",payload);
       });
+}
     
-}   
-
 //On-load
 window.onload = function(){
     setEditor();
-    mainMenu.createMenu();
-
-    //Doesn't work need a work around for the li's
     setMode();
+    mainMenu.createMenu();
+    
 }
 
 //Setting Stylesheet
@@ -59,6 +43,13 @@ function setStylesheet(theme){
 
 //Set Language-Mode
 function setMode(){
+    exports.editableCodeMirror.getOption("mode");
+    //Check CodeMirror Mode
+    if(infoBarLanguageMode.innerHTML == "" ){
+        infoBarLanguageMode.innerHTML = "None"
+    }else{
+        infoBarLanguageMode.innerHTML = exports.editableCodeMirror.getOption("mode");
+    }
     infoBarLanguageMode.addEventListener("click",function(){
         
         //Check for visibility of dialog
@@ -72,16 +63,15 @@ function setMode(){
             languageListElements[i].addEventListener("click",function(){
                 //Set CodeMirror Mode
                 var newMode = this.textContent.toLowerCase();
-                exports.editableCodeMirror.setOption("mode","'"+newMode+"'");
+                exports.editableCodeMirror.setOption("mode",newMode);
                 languageModeDialog.style.display ="none";
                 infoBarLanguageMode.innerHTML = newMode.toLocaleUpperCase();
-                
         });
     }
-    });
-
-       
+    });      
 }
+
+
         
    
 
